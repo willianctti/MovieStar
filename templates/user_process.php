e7883975cbc1401094a96e4b5a033d9b5dbafbe6
willianctti <willianctti@gmail.com>
2023-10-18 15:17:28 -0300

    updates
    16- Alterando Header e rota ao usuário realizar login
    17- Logout
    18- Formulário de edição de informações do usuário
    19- Formulário de edição de senha
    20- Edição de usuário **Inserção no DT
    21- Upload de imagem do User

---
+++ templates/user_process.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once("globals.php");
+require_once("db.php");
+require_once("models/User.php");
+require_once("models/Message.php");
+require_once("dao/UserDAO.php");
+
+$message = new Message($BASE_URL);
+
+$userDao = new UserDAO($conn, $BASE_URL);
+
+// Resgata o tipo do formulário
+$type = filter_input(INPUT_POST, "type");
+
+// Atualizar usuário
+if($type === "update") {
+
+    //Resgata dados do usuário
+    $userData = $userDao->verifyToken();
+
+    //Receber dados do POST
+    $name = filter_input(INPUT_POST, "name");
+    $lastname = filter_input(INPUT_POST, "lastname");
+    $email = filter_input(INPUT_POST, "email");
+    $bio = filter_input(INPUT_POST, "bio");
+
+    // Criar um novo ovjeto de usuário
+    $user = new User();
+
+    //Preencher os dados do usuário
+    $userData->name = $name;
+    $userData->lastname = $lastname;
+    $userData->email = $email;
+    $userData->bio = $bio;
+    
+    $userDao->update($userData);
+
+
+// Atualizar senha do usuário
+}else if ($type === "changepassword") {
+
+} else {
+    $message->setMessage("Informações inválidas!", "error", "index.php");
+
+}
+
